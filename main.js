fe395f70f1f4bde01b500f69c67a1cdd27e1120b
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-08-27 15:16:28 +0200

    js

--- main.js
+++ main.js
@@ -55,6 +55,57 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
-function myFunction() {
-    document.getElementById("click").click();
-  }
+const postList = document.getElementById('container');
+
+for (let i = 0; i < posts.length; i++) {
+    postList.innerHTML += `<div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${posts[i].content}</div>
+            <div class="post__image">
+                <img src="${posts[i].media}" alt="1">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `;
+
+}
+const likeButtons = document.querySelectorAll('.js-like-button');
+console.log('likeButtons',likeButtons, likeButtons.length);
+
+for (let i = 0; i < likeButtons.length; i++){
+    likeButtons[i].addEventListener('click',function(hastag){
+        hastag.preventDefault();
+
+        this.classList.add('like-button--liked');
+
+
+        const postId = this.dataset.postId;
+        const counterElem = document.getElementById('like-counter-' + postId);
+        let likesCount = parseInt(counterElem.innerText);
+        likesCount++;
+        counterElem.innerHTML = likesCount; 
+        
+    });
+}
